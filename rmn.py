c7d9bc0057a955766aa8a12cb5b12eac3e8c311c
Mehechiger <Mehechiger@users.noreply.github.com>
2019-12-15 12:22:58 +0100

    update shell_access_rmapi

--- rmn.py
+++ rmn.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from subprocess import call
+from subprocess import Popen, PIPE
+import re
 
 
 rmapi_loc = "go run github.com/juruen/rmapi"
 
 
-def rmapi(*cmd):
-    call("echo 'cd pdt \nls' | "+rmapi_loc, shell=True)
+def rmapi(*cmds):
+    return Popen("echo '%s' | %s" % ("\n".join(cmds), rmapi_loc), stdout=PIPE, shell=True).stdout.read()
 
 
 if __name__ == "__main__":
-    rmapi()
+    print(rmapi("ls", "cd psdt", "ls"))
